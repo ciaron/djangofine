a8c65cc6f5414188ad9a6feffe0972cc6bf000ce
Ciaron Linstead <ciaron.linstead@gmail.com>
2013-11-10 17:43:51 +0100

    initial commit

---
+++ uploader/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'uploader.views.index', name='index'),
+
+)
