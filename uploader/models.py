8be9e53cf0d34d0c26f7593c6c46f0018e044bc5
Ciaron Linstead <ciaron.linstead@gmail.com>
2013-11-13 22:19:56 +0100

    django model integration first steps

--- uploader/models.py
+++ uploader/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class Image(models.Model):
-    title = models.CharField(max_length=200)
-    # image = models.ImageField()
+    title = models.CharField(max_length=60)
+    #image = models.ImageField(upload_to='uploads')
+    image = models.ImageField() # with upload_to and fineuploader, we get 2 files!
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
--- uploader/views.py
+++ uploader/views.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from uploader import settings
 from uploader import qqFileUploader
+from uploader.models import Image
+
 import os
 
 def index(request):
@@ -16,7 +18,21 @@ def index(request):
 @csrf_exempt
 def upload(request):
     uploader = qqFileUploader(request, os.path.join(settings.MEDIA_ROOT ,"upload/"), [".jpg", ".png", ".ico", ".*", ".avi"], 2147483648)
-    return HttpResponse(uploader.handleUpload())
+    
+    result = uploader.handleUpload()
+
+    # file is in request.POST (request.FILES?)
+    # how do we know if handleUpload() has dealt with chunked or normal files?
+
+    file = request.FILES['qqfile']
+    image = Image()
+    image.image = file
+    image.width = image.image.width
+    image.height = image.image.height
+    image.title = "%s_%s" % (request.POST['qquuid'], request.POST['qqfilename'])
+    image.save()
+
+    return HttpResponse(result)
 
 @csrf_exempt
 def upload_delete(request, need_to_delete):
